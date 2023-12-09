82a5d03b6ed64fd7bd1c6998382c09619c4016d7
mahdi <mahdijeljeli1@gmail.com>
2023-12-09 14:15:48 +0100

    modification css

--- src/app/Models/ActionModel.ts
+++ src/app/Models/ActionModel.ts
@@ -1,51 +1,28 @@
 export const Action=[
     {
         IdAction : 1,
-        titre :'action1',
-        date : '12/10/2023',
+        titre :'M3a jaafour',
+        date : '12/10/2019',
         description : "hello world   Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore.Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error. v  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore. Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error. Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore.Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error. v  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore. Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore.Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error. v  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore. Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore.Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error. v  Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis quidem iusto dolores ullam quo alias ab dolore. Minima quia dolor possimus, quo odio mollitia itaque autem, ipsam eius, quasi error",
-        lieu : 'algo ',
+        lieu : 'ZOUAOUINE ',
         type: "culturel",
-        URl : "assets/images/1.jpg"
+        URl : "assets/jaafour/12.jpg"
     },
     {   IdAction : 2,
-        titre :'action2',
+        titre :'Hadhra',
         description : "hello world",
-        date : '12/10/2023',
-        lieu : 'algo ',
+        date : '12/10/2020',
+        lieu : 'ZOUAOUINE ',
         type: "culturel",
-        URl : "assets/images/2.jpg"
+        URl : "assets/hadhra/14.jpg"
     },  
     {   IdAction : 3,
-        titre :'action3',
-        date : '12/10/2023',
+        titre :'Mini-Souk',
+        date : '12/10/2020',
         description : "hello world",
-        lieu : 'algo ',
-        type: "culturel",
-        URl : "assets/images/12.jpg"
-    },  
-    {   IdAction : 4,
-        titre :'action4',
-        date : '12/10/2023',
-        description : "hello world",
-        lieu : 'algo ',
-        type: "culturel",
-        URl : "assets/images/3.jpg"
-    },  
-    {   IdAction : 5,
-        titre :'action5',
-        date : '12/10/2023',
-        description : "hello world",
-        lieu : 'algo ',
-        type: "culturel",
-        URl : "assets/images/4.jpg"
-    },  
-    {   IdAction : 6,
-        titre :'action6',
-        date : '12/10/2023',
-        description : "hello world",
-        lieu : 'algo ',
-        type: "culturel",
-        URl : "assets/images/13.jpg"
-    },
+        lieu : 'ZOUAOUINE ',
+        type: "Econommique",
+        URl : "assets/minisouk/10.jpg"
+    }
+  
 ]
--- src/app/Models/Galerie.ts
+++ src/app/Models/Galerie.ts
@@ -2,56 +2,174 @@ export const Galerie=[
     {
         IdPhoto : 1,
         IdAction :1,
-        URl : "assets/images/1.jpg"
+        URl : "assets/jaafour/1.jpg"
     },
     {
         IdPhoto : 2,
         IdAction : 1,
-        URl : "assets/images/2.jpg"
+        URl : "assets/jaafour/2.jpg"
     },
     
     {
         IdPhoto : 3,
         IdAction : 1,
-        URl : "assets/images/3.jpg"
+        URl : "assets/jaafour/3.jpg"
     },
-    
     {
         IdPhoto : 4,
-        IdAction :2,
-        URl : "assets/images/4.jpg"
+        IdAction : 1,
+        URl : "assets/jaafour/4.jpg"
     },
-    
     {
         IdPhoto : 5,
-        IdAction :2,
-        URl : "assets/images/5.jpg"
+        IdAction : 1,
+        URl : "assets/jaafour/5.jpg"
     },
-    
     {
         IdPhoto : 6,
-        IdAction :2,
-        URl : "assets/images/6.jpg"
-    },
-    
-    {
+        IdAction : 1,
+        URl : "assets/jaafour/6.jpg"
+    },   {
         IdPhoto : 7,
-        IdAction :3,
-        URl : "assets/images/7.jpg"
+        IdAction : 1,
+        URl : "assets/jaafour/7.jpg"
     },
     {
         IdPhoto : 8,
-        IdAction :3,
-        URl : "assets/images/8.jpg"
+        IdAction : 1,
+        URl : "assets/jaafour/8.jpg"
+    },   {
+        IdPhoto : 9,
+        IdAction : 1,
+        URl : "assets/jaafour/9.jpg"
     },
     {
-        IdPhoto : 9,
-        IdAction :3,
-        URl : "assets/images/12.jpg"
-    },  {
         IdPhoto : 10,
+        IdAction : 1,
+        URl : "assets/jaafour/10.jpg"
+    },
+    {
+        IdPhoto : 11,
+        IdAction : 1,
+        URl : "assets/jaafour/11.jpg"
+    },
+    {
+        IdPhoto : 12,
+        IdAction :2,
+        URl : "assets/hadhra/2.jpg"
+    },
+    
+    {
+        IdPhoto : 13,
+        IdAction :2,
+        URl : "assets/hadhra/3.jpg"
+    },
+    
+    {
+        IdPhoto : 14,
+        IdAction :2,
+        URl : "assets/hadhra/4.jpg"
+    },
+    
+    {
+        IdPhoto : 15,
+        IdAction :2,
+        URl : "assets/hadhra/5.jpg"
+    },
+    {
+        IdPhoto : 16,
+        IdAction :2,
+        URl : "assets/hadhra/6.jpg"
+    },
+    {
+        IdPhoto : 17,
+        IdAction :2,
+        URl : "assets/hadhra/7.jpg"
+    },
+    {
+        IdPhoto : 18,
+        IdAction :2,
+        URl : "assets/hadhra/8.jpg"
+    }, {
+        IdPhoto : 19,
+        IdAction :2,
+        URl : "assets/hadhra/9.jpg"
+    },
+    {
+        IdPhoto : 20,
+        IdAction :2,
+        URl : "assets/hadhra/10.jpg"
+    },  {
+        IdPhoto : 21,
+        IdAction :2,
+        URl : "assets/hadhra/11.jpg"
+    },  {
+        IdPhoto : 22,
+        IdAction :2,
+        URl : "assets/hadhra/12.jpg"
+    },
+    {
+        IdPhoto : 23,
+        IdAction :2,
+        URl : "assets/hadhra/13.jpg"
+    },  {
+        IdPhoto : 24,
+        IdAction :2,
+        URl : "assets/hadhra/15.jpg"
+    },
+    {
+        IdPhoto : 25,
         IdAction :3,
-        URl : "assets/images/10.jpg"
+        URl : "assets/minisouk/1.jpg"
+    },
+    {
+        IdPhoto : 26,
+        IdAction :3,
+        URl : "assets/minisouk/2.jpg"
+    },
+    {
+        IdPhoto : 27,
+        IdAction :3,
+        URl : "assets/minisouk/3.jpg"
+    },  {
+        IdPhoto : 28,
+        IdAction :3,
+        URl : "assets/minisouk/4.jpg"
+    },
+    {
+        IdPhoto : 29,
+        IdAction :3,
+        URl : "assets/minisouk/5.jpg"
+    },
+    {
+        IdPhoto : 30,
+        IdAction :3,
+        URl : "assets/minisouk/6.jpg"
+    },
+    {
+        IdPhoto : 31,
+        IdAction :3,
+        URl : "assets/minisouk/7.jpg"
+    },
+    {
+        IdPhoto : 32,
+        IdAction :3,
+        URl : "assets/minisouk/8.jpg"
+    },
+    {
+        IdPhoto : 33,
+        IdAction :3,
+        URl : "assets/minisouk/9.jpg"
+    },
+    {
+        IdPhoto : 34,
+        IdAction :3,
+        URl : "assets/minisouk/10.jpg"
+    },
+    {
+        IdPhoto : 35,
+        IdAction :3,
+        URl : "assets/minisouk/11.jpg"
     },
     
 ]
